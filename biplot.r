b67ced9f888daafc9dcae3fb4d66bf2a0467566d
Muhammad Ghazy <muhammadghazismp85@gmail.com>
2021-06-30 22:08:11 +0700

    Updated script

--- biplot.r
+++ biplot.r
@@ -2,31 +2,15 @@ library(stats)
 library(devtools)
 install_github("vqv/ggbiplot")
 library(ggbiplot)
-library(magrittr)
-library(dplyr)
-library(ggpubr)
+library(readxl)
 
+air_data_dki <- read_excel("D:/Ghazi/Kuliah/Semester 6/MultiV 2/UAS/Tugas-akhir-Multiv2/air_data_dki.xlsx")
 df = air_data_dki
 
+#Biplot
 pc <- prcomp(x = df[-1],center = TRUE, scale. = FALSE)
 print(pc)
 summary(pc)
 plot(pc)
 screeplot(pc, type = "line", main = "Scree plot")  
 ggbiplot(pc)
-
-
-biplot = ggbiplot(pcobj = pc,
-                  choices = c(1,2),
-                  obs.scale = 1, var.scale = 1,  # Scaling of axis
-                  labels = row.names(df),     # Add labels as rownames
-                  labels.size = 4,
-                  varname.size = 5,
-                  varname.abbrev = TRUE,  # Abbreviate variable names (TRUE)
-                  var.axes = FALSE,      # Remove variable vectors (TRUE)
-                  circle = FALSE,        # Add unit variance circle (TRUE)
-                  ellipse = TRUE) # Adding ellipses
-print(biplot)
-
-
-

1c471ccd73200b3418986c7514d2f87518f52008
Muhammad Ghazy <muhammadghazismp85@gmail.com>
2021-07-01 23:17:25 +0700

    Updated Script

--- biplot.r
+++ biplot.r
@@ -3,6 +3,7 @@ library(devtools)
 install_github("vqv/ggbiplot")
 library(ggbiplot)
 library(readxl)
+library(factoextra)
 
 air_data_dki <- read_excel("D:/Ghazi/Kuliah/Semester 6/MultiV 2/UAS/Tugas-akhir-Multiv2/air_data_dki.xlsx")
 df = air_data_dki
@@ -14,3 +15,24 @@ summary(pc)
 plot(pc)
 screeplot(pc, type = "line", main = "Scree plot")  
 ggbiplot(pc)
+
+
+# Get the variable coor
+eig.val = get_eigenvalue(pc)
+eig.val
+
+res.var = get_pca_var(pc)
+res.var$coord
+
+res.ind = get_pca_ind(pc)
+res.ind$coord
+
+datakoor = data.frame(pc$x)
+normdatakoor = data.frame(scale(datakoor))
+normdatakoor
+plot(
+  normdatakoor$PC1,
+  normdatakoor$PC2
+)
+write.csv(normdatakoor,'D:\\Ghazi\\Kuliah\\Semester 6\\MultiV 2\\UAS\\Tugas-akhir-Multiv2\\normkoor.csv')
+
